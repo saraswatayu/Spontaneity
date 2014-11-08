d81ebdd824619c53cd1417a0b4a2eaac32fcbbdb
saraswatayu <saraswatayu@yahoo.com>
2014-11-08 00:30:42 -0800

    Initial Commit

---
+++ Spontaneity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spontaneity
+//
+//  Created by Ayush Saraswat on 11/8/14.
+//  Copyright (c) 2014 SwatTech, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
